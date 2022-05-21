6eb08d8ede6e1e08be4878dec3461fda177ea020
RMohit927 <radadiyamohit927@gmail.com>
2022-05-21 16:30:11 +0530

    New Practice Test CustomerList Project Added

---
+++ PracticeTest/customerlist/src/App.js
@@ -0,0 +1,12 @@
+import CustomerList from "./Components/index";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return (
+    <div className="App">
+      <CustomerList />
+    </div>
+  );
+}
+
+export default App;
